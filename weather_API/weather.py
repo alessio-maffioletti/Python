7b28190da801c01568e7e44a7a4681f99c76f6e5
alessio-maffioletti <maffioletti.alessio@proton.me>
2022-06-23 21:43:47 +0200

    edited directories

---
+++ weather_API/weather.py
@@ -0,0 +1,20 @@
+import requests as rq
+
+print('''
+                             _                   
+                        _   | |                  
+ _ _ _  _____  _____  _| |_ | |__   _____   ____ 
+| | | || ___ |(____ |(_   _)|  _ \ | ___ | / ___)
+| | | || ____|/ ___ |  | |_ | | | || ____|| |    
+ \___/ |_____)\_____|   \__)|_| |_||_____)|_|    
+                                                 
+''')
+
+key = "e47efe1099567448b92eb3c9c12b5ae0"
+location = input('city name: ')
+
+response = rq.get(f"http://api.openweathermap.org/data/2.5/weather?appid={key}&q={location}").json()
+print(f"""
+Weather: {response['weather'][0]['description']}
+Temperature: from {round(response['main']['temp_min']/10)} to {round(response['main']['temp_max']/10)}
+""")
