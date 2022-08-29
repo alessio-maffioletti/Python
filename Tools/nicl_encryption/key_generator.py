5034e70b0e4c58a6a05ce8a66472b9c6ca78f27b
BuildTools <unconfigured@null.spigotmc.org>
2022-08-29 19:05:13 +0200

    added file output

--- Tools/nicl_encryption/key_generator.py
+++ Tools/nicl_encryption/key_generator.py
@@ -1,15 +1,34 @@
 import random
+import sys
+import argparse as arg
 
-#define character to use
-chars = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%$^&*()_+}{|"
-#make character list
-char_list = []
-for char in chars:
-    char_list.append(char)
-#shuffle list
-random.shuffle(char_list)
-#print in user readable format
-key = ""
-for char in char_list:
-    key += char
-print(f"\"{key}\"")
+def main():
+    parser = arg.ArgumentParser()
+    parser.add_argument('--output', help="output key to file")
+    args = parser.parse_args()
+    #define character to use
+    chars = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%$^&*()_+}{|"
+    
+    #make character list
+    char_list = []
+    for char in chars:
+        char_list.append(char)
+    
+    #shuffle list
+    random.shuffle(char_list)
+    
+    #make in user readable format
+    key = ""
+    for char in char_list:
+        key += char
+    
+    #output key into file or terminal
+    
+    if args.output is not None:
+        with open(f'{args.output}', 'w+') as f:
+            f.write(key)
+    else:
+        print(f"\"{key}\"")
+
+if __name__ == "__main__":
+    sys.exit(main())
