7ef25fe845175d8e11a5955878ed1e5051713d08
BuildTools <unconfigured@null.spigotmc.org>
2022-08-29 19:20:03 +0200

    added fileread in encryption and decryption

--- Tools/nicl_encryption/decrypt_2.py
+++ Tools/nicl_encryption/decrypt_2.py
@@ -4,13 +4,22 @@ import argparse as arg
 def main():
     parser = arg.ArgumentParser()
     parser.add_argument('msg', help="message to decrypt")
-    parser.add_argument('key', help="key used in encryption")
+    parser.add_argument('--key', help="key used in encryption")
+    parser.add_argument('--keyfile', help="keyfile used in encrypyion")
     args = parser.parse_args()
     
     #make key a list
     key = []
-    for char in args.key:
-        key.append(char)
+    #check if the key is printed
+    if args.key is not None:
+        for char in args.key:
+            key.append(char)
+    #check if key is a file
+    if args.keyfile is not None:
+        with open(args.keyfile, 'r') as f:
+            raw_key = f.read()
+        for char in raw_key:
+            key.append(char)
 
     #convert encrypted message to list of indexes of the key
     alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
--- Tools/nicl_encryption/encrypt_2.py
+++ Tools/nicl_encryption/encrypt_2.py
@@ -4,14 +4,23 @@ import argparse as arg
 def main():
     parser = arg.ArgumentParser()
     parser.add_argument('msg', help = "message to encrypt", type=str)
-    parser.add_argument('key', help = "key for encryption")
+    parser.add_argument('--key', help = "key for encryption")
+    parser.add_argument('--keyfile', help = "keyfile for encryption")
     args = parser.parse_args()
     
-    #make key readable
+    #make key a list
     key = []
-    for char in args.key:
-        key.append(char)
-    
+    #check if the key is printed
+    if args.key is not None:
+        for char in args.key:
+            key.append(char)
+    #check if key is a file
+    if args.keyfile is not None:
+        with open(args.keyfile, 'r') as f:
+            raw_key = f.read()
+        for char in raw_key:
+            key.append(char)
+
     #convert message to indexes
     index_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
     indexes = []
